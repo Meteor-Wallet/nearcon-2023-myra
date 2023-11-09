b8110be601fdfc3dc17fa0f0a789d7878db8cff0
Paul Myburgh <lostpebble@protonmail.com>
2023-11-09 14:41:24 +0000

    feat: got bigquery to run the sql query

--- app/api/executeSql/route.ts
+++ app/api/executeSql/route.ts
@@ -1,8 +1,9 @@
-import { executeSql } from '@/llm/alexander/executeSql';
-import { NextResponse } from 'next/server';
+import {executeSql} from '@/llm/alexander/executeSql';
+import {NextResponse} from 'next/server';
 
 export async function POST(request: Request, response: Response) {
-    const { sql } = await request.json();
+    const {sql} = await request.json();
+    console.log("Executing SQL", sql);
 
     const result = await executeSql(sql);
 
